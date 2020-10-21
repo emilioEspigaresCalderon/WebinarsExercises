a231d4635670c0daef7ef406a7f9cb698018bfc5
unknown <emilioespigares@paucasesnovescifp.cat>
2020-10-21 12:40:46 +0200

    Ninth commit
    Exercise 8 done and tested.

---
+++ WinformsExercise8/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WinformsExercise8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.students = new System.Windows.Forms.ListBox();
+            this.classroom1 = new System.Windows.Forms.Button();
+            this.classroom2 = new System.Windows.Forms.Button();
+            this.studentSelected = new System.Windows.Forms.TextBox();
+            this.clearList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // students
+            // 
+            this.students.FormattingEnabled = true;
+            this.students.ItemHeight = 16;
+            this.students.Location = new System.Drawing.Point(136, 40);
+            this.students.Name = "students";
+            this.students.Size = new System.Drawing.Size(120, 196);
+            this.students.TabIndex = 0;
+            this.students.SelectedIndexChanged += new System.EventHandler(this.StudentSelected);
+            // 
+            // classroom1
+            // 
+            this.classroom1.Location = new System.Drawing.Point(136, 271);
+            this.classroom1.Name = "classroom1";
+            this.classroom1.Size = new System.Drawing.Size(120, 23);
+            this.classroom1.TabIndex = 1;
+            this.classroom1.Text = "Classroom 1";
+            this.classroom1.UseVisualStyleBackColor = true;
+            this.classroom1.Click += new System.EventHandler(this.classroom1_Click);
+            // 
+            // classroom2
+            // 
+            this.classroom2.Location = new System.Drawing.Point(136, 328);
+            this.classroom2.Name = "classroom2";
+            this.classroom2.Size = new System.Drawing.Size(120, 23);
+            this.classroom2.TabIndex = 2;
+            this.classroom2.Text = "Classroom 2";
+            this.classroom2.UseVisualStyleBackColor = true;
+            this.classroom2.Click += new System.EventHandler(this.classroom2_Click);
+            // 
+            // studentSelected
+            // 
+            this.studentSelected.Location = new System.Drawing.Point(339, 40);
+            this.studentSelected.Name = "studentSelected";
+            this.studentSelected.Size = new System.Drawing.Size(316, 22);
+            this.studentSelected.TabIndex = 3;
+            // 
+            // clearList
+            // 
+            this.clearList.Location = new System.Drawing.Point(136, 379);
+            this.clearList.Name = "clearList";
+            this.clearList.Size = new System.Drawing.Size(120, 23);
+            this.clearList.TabIndex = 4;
+            this.clearList.Text = "Clear list";
+            this.clearList.UseVisualStyleBackColor = true;
+            this.clearList.Click += new System.EventHandler(this.clearList_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clearList);
+            this.Controls.Add(this.studentSelected);
+            this.Controls.Add(this.classroom2);
+            this.Controls.Add(this.classroom1);
+            this.Controls.Add(this.students);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox students;
+        private System.Windows.Forms.Button classroom1;
+        private System.Windows.Forms.Button classroom2;
+        private System.Windows.Forms.TextBox studentSelected;
+        private System.Windows.Forms.Button clearList;
+    }
+}
+
---
+++ WinformsExercise8/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformsExercise8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void classroom1_Click(object sender, EventArgs e)
+        {
+            students.Items.Clear();
+            students.Items.Add("Josep");
+            students.Items.Add("Marina");
+            students.Items.Add("Aina");
+        }
+
+        private void classroom2_Click(object sender, EventArgs e)
+        {
+            students.Items.Clear();
+            students.Items.Add("Joan");
+            students.Items.Add("Xesc");
+            students.Items.Add("Paula");
+        }
+
+        private void clearList_Click(object sender, EventArgs e)
+        {
+            students.Items.Clear();
+        }
+
+        private void StudentSelected(object sender, EventArgs e)
+        {
+            studentSelected.Text = students.SelectedItem.ToString();
+        }
+    }
+}
